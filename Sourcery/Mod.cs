dc10da41e3b67c2bcf11031969c94ad6fee73348
Millzy <millzyyt08@gmail.com>
2023-12-28 21:40:30 +0000

    base project

---
+++ Sourcery/Mod.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+
+#if !QUEST
+using Microsoft.Win32;
+#endif
+
+namespace Sourcery
+{
+    public class Mod : MelonMod
+    {
+#if !QUEST
+        private const string _modRegistryPath = @"HKEY_CURRENT_USER\BONELAB Mods\Sourcery";
+        private const string _noTutorialKey = "noTutorial";
+#endif
+
+        public override void OnInitializeMelon()
+        {
+#if !QUEST
+            if (Registry.GetValue(_modRegistryPath, _noTutorialKey, null) == null)
+                Registry.SetValue(_modRegistryPath, _noTutorialKey, 0, RegistryValueKind.DWord);
+#else
+            // TODO: Quest tutorial check
+#endif
+        }
+    }
+}
---
+++ Sourcery/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Sourcery;
+using BuildInfo = Sourcery.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(null!)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Copyright (c) 2023 Frederick (Millzy) Mills")]
+[assembly: AssemblyTrademark(null)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(BuildInfo.Version + ".*")]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: MelonInfo(typeof(Mod), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author)]
+
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonID(BuildInfo.Id)]
+
+[assembly: VerifyLoaderVersion("0.5.7")]
